9e755a1f80c4a30abce81590ded930f4ae1f4ed3
RomanBuletsa <r.buletsa@gmail.com>
2019-06-20 19:20:28 +0300

    Added basic architecture

---
+++ Assets/Scripts/Application/ApplicationManager.cs
@@ -0,0 +1,26 @@
+﻿using Game;
+using MainMenu;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Application
+{
+    public class ApplicationManager : MonoBehaviour
+    {
+        public static ApplicationManager Instance { get; private set; }
+        
+        public MainMenuManager MainMenuManager { get; set; }
+        public GameManager GameManager { get; set; }
+        
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            Instance = this;
+        }
+        
+        private void Start()
+        {
+            SceneManager.LoadScene(ApplicationScenes.MainMenu.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using Application;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private Button menuButton;
+    
+        private void Awake()
+        {
+            ApplicationManager.Instance.GameManager = this;
+            
+            menuButton.onClick.AddListener(OnMenuButtonClicked);
+
+        }
+    
+        private void OnMenuButtonClicked() => SceneManager.LoadScene(ApplicationScenes.MainMenu.ToString());
+
+        private void OnDestroy()
+        {
+            ApplicationManager.Instance.GameManager = null;
+        }
+    }
+}
