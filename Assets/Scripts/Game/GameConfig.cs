2aef27fc066008e22b27123ca04c86274d54ea56
Julianna2909 <32294638+Julianna2909@users.noreply.github.com>
2019-06-22 10:17:05 +0300

    Added gameconfig, assets, craated sampel of fallingobject

---
+++ Assets/Scripts/Game/GameConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu]
+    public class GameConfig : ScriptableObject
+    {
+        [SerializeField] private List<Round> rounds;
+        [SerializeField] private List<FallingObject> fallingObjects;
+
+        public List<Round> Rounds => rounds;
+        public List<FallingObject> FallingObjects => fallingObjects;
+    }
+}
---
+++ Assets/Scripts/Game/ItemMover.cs
@@ -0,0 +1,25 @@
+﻿using Application;
+using UnityEngine;
+
+namespace Game
+{
+    public class ItemMover : MonoBehaviour
+    {
+        private Transform transform;
+        private Rigidbody2D rigidbody;
+        private float speed;
+
+        private void Awake()
+        {
+            transform = gameObject.GetComponent<Transform>();
+            rigidbody = gameObject.GetComponent<Rigidbody2D>();
+            speed = ApplicationManager.Instance.GameManager.GetCurrentRound().Speed;
+            rigidbody.velocity =  speed * Vector2.down;
+        }
+
+        private void Update()
+        {
+            transform.Rotate(Vector2.up * 250f * Time.deltaTime);
+        }
+    }
+}
