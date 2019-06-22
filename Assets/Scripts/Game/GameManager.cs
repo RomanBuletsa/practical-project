d6479a1b546dd23e4727ad1ea5a5e8dbb2529824
RomanBuletsa <r.buletsa@gmail.com>
2019-06-22 21:47:49 +0300

    Added spawn, caught, playerControl

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,23 +1,142 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Application;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = System.Random;
 
 namespace Game
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private Caughting caughting;
+        [SerializeField] private Land land;
         [SerializeField] private Button menuButton;
         [SerializeField] private GameConfig gameConfig;
+        [SerializeField] private Transform parentSpawn;
+        [SerializeField] private Transform[] positionSpawn;
+        [SerializeField] private  TextMeshProUGUI countText;
+        [SerializeField] private  TextMeshProUGUI targetText;
+
+        
 
         private Round currentRound;
-    
+        private int roundNumber;
+        private int countFallingObjects=0;
+        private Queue<TypesPrefabs> objectsQueue = new Queue<TypesPrefabs>();
+        private Dictionary<TypesPrefabs,List<GameObject>> bjectsDictionary = new Dictionary<TypesPrefabs, List<GameObject>>();
+        private Random rand;
+        private int currentCount;
+        private Coroutine coroutine;
+        
         private void Awake()
         {
             ApplicationManager.Instance.GameManager = this;
             
             menuButton.onClick.AddListener(OnMenuButtonClicked);
+            caughting.Caught += Caught;
+            land.Delete += Delete;
+        }
+
+        private void Start()
+        {
+            StartGame();
+        }
+        
+        private void StartGame()
+        {
+            roundNumber = 0;
+            UpdateCountText();
+            StartRound();
+        }
+
+        private void StartRound()
+        {
+            currentRound = gameConfig.Rounds.ElementAt(roundNumber);
+            UpdateTargetText();
+            bjectsDictionary.Clear();
+            foreach (TypesPrefabs type in currentRound.AllObject)
+            {
+                foreach (FallingObject obj in gameConfig.FallingObjects)
+                {
+                    if (type.Equals(obj.Type))
+                    {
+                        bjectsDictionary.Add(type,obj.Prefabs);
+                    }
+                }
+            }
+
+            coroutine = null;
+            coroutine = StartCoroutine(SpawnCoroutine());
+            
+        }
+
+        private IEnumerator SpawnCoroutine()
+        {
+            float timeElapsed = 0f;
+            while (countFallingObjects<=currentRound.CountFallingObjPerPound)
+            {
+                if (timeElapsed >= currentRound.RoundSpawnRate)
+                {
+                    timeElapsed = 0f;
+                    rand = new Random();
+                    int pos1 = rand.Next(0, positionSpawn.Length);
+                    int pos2 = rand.Next(0, currentRound.AllObject.Count);
+                    TypesPrefabs typ = currentRound.AllObject[pos2];
+                    objectsQueue.Enqueue(typ);
+                    int pos3 = rand.Next(0, bjectsDictionary[typ].Count);
+                    var newObj = Instantiate(bjectsDictionary[typ][pos3],parentSpawn);
+                    newObj.transform.position = positionSpawn[pos1].position;
+                    
+                    
+                    countFallingObjects++;
+                }
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            countFallingObjects = 0;
+            NextRound();
+
+        }
+
+        private void Delete()
+        {
+            if(objectsQueue.Count>0)objectsQueue.Dequeue();
+        }
+
+        private void Caught()
+        {
+            if(objectsQueue.Count>0)
+            if (objectsQueue.Dequeue().Equals(currentRound.ObjectsToCatch))
+                currentCount += currentRound.TrueAnswerPrice;
+            else currentCount += currentRound.FalseAswerPrice;
+            UpdateCountText();
+        }
+        
+        private void UpdateCountText()
+        {
+            countText.text = $"{currentCount}";
+        }
+        
+        private void UpdateTargetText()
+        {
+            targetText.text = currentRound.ObjectsToCatch.ToString();
+        }
+
+
+        private void NextRound()
+        {
+            StopCoroutine(coroutine);
+            roundNumber++;
+            if (roundNumber < gameConfig.Rounds.Count)
+            {
+                StartRound();
+            }
         }
 
         public Round GetCurrentRound() => currentRound;
---
+++ Assets/Scripts/Game/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private bool l = false;
+    private bool r = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0f) l = true;
+        if (Input.GetAxis("Horizontal") < 0f) r = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (l)
+        {
+            transform.Translate(Vector3.right * 0.2f);
+            l = false;
+        }
+
+        if (r)
+        {
+            transform.Translate(Vector3.left * 0.2f);
+            r = false;
+        }
+    }
+}
